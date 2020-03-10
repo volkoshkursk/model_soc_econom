be4604008f5e4b224902019f02f3cf134ab612e6
AlexanderNt <alexandr.spy@gmail.com>
2020-03-10 21:05:26 +0300

    Made images properly working(at least in private browser mode) and other misc fixes

--- docker/web_docker/app/hello.py
+++ docker/web_docker/app/hello.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, Response, render_template, redirect, request
+from flask import Flask, Response, render_template, redirect, request, url_for
 from app.forms import CheckForm
 import matplotlib.pyplot as plt
 import os
 import requests as req
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/app/static')
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
  
 @app.route("/",methods=['GET', 'POST'])
 def home():
     form = CheckForm()
+
+    hist_path =  "app/static/hist.png"
+    # Deleting old image
+    if os.path.isfile(hist_path):
+        print("deleting")
+        os.remove(hist_path)
+
     if form.validate_on_submit():
         resp = req.get(url='http://script:8990', params = dict(good=form.good_name.data,unit=form.unit.data,price=form.price.data))
         data = resp.json()
         # Saving image to show 
-        hist_path = "app/templates/hist.png"
-        print(os.listdir("/app"))
-        # Deleting old image
-        if os.path.isfile(hist_path):
-            print("deleting")
-            os.remove(hist_path)
+        hist_path =  "app/static/hist.png"
+        
         plt.hist(data['hist'])
         plt.savefig(hist_path)
+        plt.close()
+
         # Making normal answer
         if data['res'] == 1:
             site_answer = "Probably snached"
@@ -36,14 +42,14 @@ def home():
         return render_template('result.html', title ='Result',result=site_answer)
     return render_template('check.html', title='Snached of not??', form=form)
 
-#@app.route("/result")
-#def result():
-#    res = request.args.get('res')
-#    data = eval(str(base64.b64decode(bytes(request.args.get('data'), 'utf8'))))
-#    if (data != -1):
-#        plt.hist(data)
-#        plt.savefig("/static/hist.png")
-#    return render_template('result.html',title = "bla",result = res)
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 app.run('0.0.0.0', port=8989, debug=True)
 
